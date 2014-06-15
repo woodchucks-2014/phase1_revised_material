4c25842242da12c92a2066b645443b8a83b31a5b
Vivek M George <null>
2014-06-14 23:51:16 -0400

    updated cookies and mock

--- revised_mock_vivek.rb
+++ revised_mock_vivek.rb
@@ -1,20 +1,117 @@
+# require 'CSV'
+
+# data_array = []
+
+# CSV.foreach('students.csv') do |row|
+#   data_array << row
+
+# end
+
+# student_names = data_array.map{|column| column[1] if column[1] !="first_name"}
+# student_names.delete(nil)
+
+# new_names = ["Vivek", "Mike", "Chris"]
+# new_names.each{|name| student_names.push(name)}
+# p student_names
+
+# def add_number
+#   students_names.each_with_index do |name, index|
+#     puts "#{(index +1)} " + name
+#   end
+# end
+
+# add_number
+
+
 require 'CSV'
 
-data_array = []
+class List
+attr_accessor :data_array, :cohort
 
-CSV.foreach('students.csv') do |row|
-  data_array << row
+  def initialize(csv)
+    @data_array = []
+    File.open(csv, mode='a+'){|file| file.each_line{|line| @data_array << Students.new(line) }}
+    @columns = @data_array.shift
+    # @cohort = cohort || "NA" # NOT WORKING
+    # p @column
+    # p @data_array
+    # p @data_array[2]
+  end
+
+  def student_ids
+    ids = []
+    @data_array.each{ |student| ids << student.id}
+  end
+
+  def first_names
+    first_name = []
+    @data_array.each{ |student| first_name << student.first_name}
+    # p first_name
+  end
+
+  def name_sorter # NOT WORKING
+    @data_array.sort_by!{ |student| student.first_name }
+  end
+
+  def name_beginning_with(*letters)
+    names_with_letter = []
+    letters.each{ |letter| @data_array.each{ |student| names_with_letter << student.first_name if student.first_name[0] == letter}}
+    names_with_letter
+  end
+
+   def phone (*phones) # NOT WORKING
+    @data_array.map!.with_index do |student, i|
+      student.phone = phones[i % phones.length] if student.phone == "NA"
+    end
+  end
+
+  def failing
+    @failers = []
+    @data_array.each{ |student| @failers << student if student.grade.to_i < 60 }
+    @failers.each{|student| puts "#{student.first_name}" }
+  end
+
+  def write(csv, *item)
+    File.open(csv,"w"){ |file| puts item.flatten}
+  end
+
 end
 
-student_names = data_array.map{|column| column[1] if column[1] !="first_name"}
-student_names.delete(nil)
+class Students
+  def initialize(student_details)
+    @student_details = student_details
+    @student_details = @student_details.gsub!("\n","").split(",")
+    # @student_info
+  end
 
-new_names = ["Vivek", "Mike", "Chris"]
-new_names.each{|name| student_names.push(name)}
-p student_names
+  def id
+    @student_details[0]
+  end
+
+  def first_name
+    @student_details[1]
+  end
+
+  def grade
+    @student_details[5]
+  end
 
 
 
+end
+
+
+
+
+# Controller
+roster = List.new('students.csv')
+# p roster
+roster.first_names
+# roster.name_sorter
+# roster.name_beginning_with("R")
+# roster.failing
+# roster.write("newstudent.csv", roster.failing)
+# p roster.phone("iOS,","Android", "Windows")
 
 
 
---
+++ vivek_revised_cookies.rb
@@ -0,0 +1,76 @@
+# MODEL
+class Oven
+  def self.turn_oven_on
+    Controller.how_cooked
+  end
+
+  def self.how_cooked
+
+  end
+
+end
+# VIEW
+class Display
+
+  attr_accessor :cookies
+  def welcome
+    puts "Hi, how are you?! Do you want to bake any cookies today?"
+    @cookies = gets.chomp
+    Controller.responses(@cookies)
+  end
+
+  def self.feedback
+    puts "Cool talk to you later!"
+  end
+
+  def self.texture
+    puts "How you liked to cooked? Doughy or Crispy?"
+    @texture = gets.chomp
+    Controller.texture_response(@texture)
+  end
+
+  def self.texture_doughy
+    puts "it will be 5 minutes"
+  end
+
+  def self.texture_crispy
+    puts "it will be 7 minutes"
+  end
+
+end
+
+# CONTROLLER
+class Controller
+  def initialize
+    @oven = Oven.new
+    @display = Display.new
+    @display.welcome
+  end
+
+  def self.responses(cookies)
+    if cookies == "yes"
+      Oven.turn_oven_on
+    else
+      Display.feedback
+    end
+  end
+
+  def self.how_cooked
+    Display.texture
+  end
+
+  def self.texture_response(texture)
+    if texture = "doughy"
+      Display.texture_doughy
+    else
+      Display.texture_crispy
+    end
+  end
+
+
+
+
+
+
+end
+start = Controller.new
