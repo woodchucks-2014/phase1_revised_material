ba3dc8f5361e74e91423fd1a7b8ab98f47b9fd66
Matthew Bunday <zenceph@gmail.com>
2014-06-17 23:32:26 -0400

    Refactored to make printing DRYer add the first letter select

--- mock_revised_vivek_v2.rb
+++ mock_revised_vivek_v2.rb
@@ -46,13 +46,17 @@ class School
     @students += students
   end
 
-  def print_students_alphabetical
-    @students.sort_by { |student| student.first_name }.each {|student| puts student}
+  # id is a sequential number...
+  def print_students(students = @students)
+    students.each {|student| puts student}
   end
 
-  # id is a sequential number...
-  def print_students
-    @students.each {|student| puts student}
+  def print_students_alphabetical
+    print_students @students.sort_by {|student| student.first_name}
+  end
+
+  def print_students_selected_by_first_name_first_letter(letter)
+    print_students @students.select {|student| student.first_name.start_with? letter}
   end
 
   def export_students_csv
@@ -72,4 +76,5 @@ school = School.new
 school.add_students(students)
 school.print_students
 school.print_students_alphabetical
+school.print_students_selected_by_first_name_first_letter('M')
 school.export_students_csv
