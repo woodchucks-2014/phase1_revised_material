ca50f5d635d31720e4c1176008296e42bb9e38c6
Matthew Bunday <zenceph@gmail.com>
2014-06-17 23:09:54 -0400

    Loading from CSV to array of hash metaprogram student class.

--- mock_revised_vivek_v2.rb
+++ mock_revised_vivek_v2.rb
@@ -1,114 +1,26 @@
-class School
-	attr_accessor :new_student_list
-	def initialize
-		@student_names = []
-		@new_students = ["Vivek", "Sandip", "Matt"]
-		file = File.open('students.csv')
-		file.each_line do |row|
-			row_array = row.split(',')
-			@student_names << row_array[1]
-		end
-		@student_names.shift
-		@student_names
-		@student_names << @new_students
-		@new_student_list = @student_names.flatten
+require "csv"
 
-	end
+module Utils
+  def self.hashes_from_csv(file)
+    rows = CSV.read(file, { headers: true }) # Set headers true so we get back CSV::Row objects instead of Array objects
+    rows.map {|row| self.symbolize_hash_keys(row.to_hash)}
+  end
 
-
-	def add_number
-		@new_student_list.each_with_index do |name, index|
-		puts "#{index + 1} #{name}"
-		end
-	end
-
-	def sort
-		@new_student_list.sort
-
-	end
-
-	def by_letter(letter)
-		@sort_names = []
-		# @new_student_list.select { |name| name[0] = letter }
-		# p name[0]
-		@new_student_list.each do |name|
-		 if name[0].downcase == letter.downcase
-		 		@sort_names << name
-			end
-		end
-		@sort_names
-	end
-
-	def display_by_cohort(cohort)
-		@new_student_list.each do |student|
-			if student.first[1] == cohort
-				p student.first[0]
-			end
-		end
-	end
-
-	def failing
-		@failing_students = []
-		new_file = File.open('students.csv')
-		new_file.each_line do |row|
-			row_array = row.split(',')
-			row_array[5].gsub!("\n","")
-			new_row_array = row_array
-
-			if new_row_array[5].to_i< 60
-			@failing_students << new_row_array[1]
-			end
-		end
-		p @failing_students
-	end
-
+  # Taken from http://stackoverflow.com/questions/800122/best-way-to-convert-strings-to-symbols-in-hash
+  def self.symbolize_hash_keys(hash)
+    Hash[hash.map {|key, value| [key.to_sym, value]}]
+  end
 end
-
-
 
 class Student
-	attr_accessor 
-	def initialize
-	
-	end
+  ATTRIBUTES = %i(id first_name last_name email phone grade) # short hand to get array of keys similar to %w
 
-	def add_cohort(school, cohort)
-		school.new_student_list.map! do |name|
-			{name => cohort.sample}
-		end
-		school.new_student_list
-	end
+  ATTRIBUTES.each do |attr|
+    define_method(attr) { @data[attr] }
+    define_method("#{attr}=") { |value| @data[attr] = value }
+  end
 
-	def add_phone(school, phone)
-		school.new_student_list.map! do |name|
-			{name => phone.sample}
-		end
-		p school.new_student_list
-	end
-
-
+  def initialize(args)
+    @data = args
+  end
 end
-
-
-# Driver code
-
-##### Release 0
-dbc = School.new
-student = Student.new
-
-##### Release 1
-# dbc.add_number
-# puts DBC.sort
-# p DBC.by_letter("a")
-
-##### Release 2
-# completed
-
-# Release 3
-# cohort = ["wind", "earth", "fire" ]
-# student.add_cohort(dbc, cohort)
-# phone = ["iPhone", "Android", "Windows" ]
-# student.add_phone(dbc, phone)
-# dbc.display_by_cohort("wind")
-dbc.failing
-
